4642cc71b44059372dc5cdf426dbe86ce22347b5
lol9519 <32627694+lol9519@users.noreply.github.com>
2019-01-13 17:24:14 -0600

    inicializandorepo

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'id8442907_dev' );             // Set database user
+  define( 'DB_PASS', 'contraizad' );             // Set database password
+  define( 'DB_NAME', 'id8442907_data' );        // Set database name
+?>
