ad2f5cf480cf9a6e280904e2ad0f0bf72fd2e583
lol9519 <32627694+lol9519@users.noreply.github.com>
2019-01-15 00:14:06 -0600

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'id8442907_dev' );             // Set database user
-  define( 'DB_PASS', 'contraizad' );             // Set database password
-  define( 'DB_NAME', 'id8442907_data' );        // Set database name
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'baseejemplo' );        // Set database name
 ?>
