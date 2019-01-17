67f632ab66204c9627fccda6e22280400efd2219
lol9519 <32627694+lol9519@users.noreply.github.com>
2019-01-16 22:16:27 -0600

    Update menu.php

--- layouts/menu.php
+++ layouts/menu.php
@@ -1,3 +1,57 @@
+<?php
+  $page_title = 'Lista de productos';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+  $products = join_product_table();
+?>
 <div>
-  <img src="/tpisocial/uploads/products/filter.jpg">
+<div>
+   <h1><p>Menú del día</p></h1>
+ <div class="panel-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th class="text-center" style="width: 50px;">#</th>
+                <th> Imagen</th>
+                <th> Descripción </th>
+                <th class="text-center" style="width: 10%;"> Categoría </th>
+                <th class="text-center" style="width: 10%;"> Stock </th>
+                <th class="text-center" style="width: 10%;"> Precio de compra </th>
+                <th class="text-center" style="width: 10%;"> Precio de venta </th>
+                <th class="text-center" style="width: 10%;"> Agregado </th>
+                <th class="text-center" style="width: 100px;"> Acciones </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($products as $product):?>
+              <tr>
+                <td class="text-center"><?php echo count_id();?></td>
+                <td>
+                  <?php if($product['media_id'] === '0'): ?>
+                    <img class="img-avatar img-circle" src="uploads/products/no_image.jpg" alt="">
+                  <?php else: ?>
+                  <img class="img-avatar img-circle" src="uploads/products/<?php echo $product['image']; ?>" alt="">
+                <?php endif; ?>
+                </td>
+                <td> <?php echo remove_junk($product['name']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['categorie']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['quantity']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['buy_price']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['sale_price']); ?></td>
+                <td class="text-center"> <?php echo read_date($product['date']); ?></td>
+                <td class="text-center">
+                  <div class="btn-group">
+                    <a href="edit_product.php?id=<?php echo (int)$product['id'];?>" class="btn btn-info btn-xs"  title="Editar" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-edit"></span>
+                    </a>
+                     <a href="delete_product.php?id=<?php echo (int)$product['id'];?>" class="btn btn-danger btn-xs"  title="Eliminar" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-trash"></span>
+                    </a>
+                  </div>
+                </td>
+              </tr>
+             <?php endforeach; ?>
+            </tbody>
+          </table>
+        </div>
 </div>
